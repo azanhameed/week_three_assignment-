8fc0a264235fa25799bf1c6286f6b9f09c9d12e6
Azan Hameed <soomroazanhameed44@gmail.com>
2026-02-19 00:00:58 +0500

    Week Three Assignment Submission

---
+++ weekthreeassignment/src/ColorList.jsx
@@ -0,0 +1,13 @@
+function ColorList({ colors }) {
+  return (
+    <ul>
+      {colors.map((color, index) => (
+        <li key={index} style={{ color: color }}>
+          {color}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ColorList;
